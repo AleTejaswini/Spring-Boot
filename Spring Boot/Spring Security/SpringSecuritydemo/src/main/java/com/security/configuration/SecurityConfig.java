641603adf8dbef541f02f92107be9df171b3caf9
AleTejaswini <aletejaswini@gmail.com>
2026-01-08 18:19:35 +0530

    Spring security example

--- Spring Boot/Spring Security/SpringSecuritydemo/src/main/java/com/security/configuration/SecurityConfig.java
+++ Spring Boot/Spring Security/SpringSecuritydemo/src/main/java/com/security/configuration/SecurityConfig.java
@@ -5,6 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -14,12 +18,27 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain securityfilterchain(HttpSecurity http) throws Exception{
-		http.authorizeHttpRequests(auth->auth
-				.requestMatchers("/public/**")
-				.permitAll().anyRequest().authenticated())
-				.httpBasic(Customizer.withDefaults());
+		return http
+		.csrf(customizer ->customizer.disable())
+		.authorizeHttpRequests(request->request.anyRequest().authenticated())
+				.httpBasic(Customizer.withDefaults())
+				.build();
+				
+	}
 	
-		return http.build();
+	@Bean
+	public UserDetailsService userservicedetail() {
+		UserDetails user1 =User.withUsername("teja")
+				.password("teja6666")
+				.roles("USER")
+				.build();
+		UserDetails user2 =User.withDefaultPasswordEncoder()
+				.username("tejaswini")
+				.password("teja")
+				.roles("admin")
+				.build();
+		
+		return new InMemoryUserDetailsManager(user1,user2);
 	}
 
 }
--- Spring Boot/Spring Security/SpringSecuritydemo/src/main/java/com/security/controller/Controllerdemo.java
+++ Spring Boot/Spring Security/SpringSecuritydemo/src/main/java/com/security/controller/Controllerdemo.java
@@ -1,7 +1,16 @@
 package com.security.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 public class Controllerdemo {
@@ -20,4 +29,7 @@ public class Controllerdemo {
 	public String secure() {
 		return "Hello public - with login";
 	}
+	
+	
+    
 }
